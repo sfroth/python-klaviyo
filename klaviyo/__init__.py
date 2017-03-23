07c25741dcb627b0a221bc177839fe6f1caa117e
Steve Roth <steve@sidestudios.com>
2017-03-23 14:18:09 -0700

    Add List methods to Klaviyo api

--- klaviyo/__init__.py
+++ klaviyo/__init__.py
@@ -9,6 +9,7 @@ import requests
 KLAVIYO_API_SERVER = 'a.klaviyo.com'
 KLAVIYO_TRACKING_ENDPOINT = 'api/track'
 KLAVIYO_IDENTIFY_ENDPOINT = 'api/identify'
+KLAVIYO_LIST_ENDPOINT = 'api/v1/list'
 KLAVIYO_DATA_VARIABLE = 'data'
 
 TRACK_ONCE_KEY = '__track_once__'
@@ -77,7 +78,21 @@ class Klaviyo(object):
             'properties' : properties,
         }, is_test)
         return self._request(KLAVIYO_IDENTIFY_ENDPOINT, query_string)
-    
+
+    def add_to_list(self, list_id, email, properties={}, confirm_optin=False):
+        params = {'api_key': self.api_token, 'email': email, 'properties': json.dumps(properties), 'confirm_optin': 'true' if confirm_optin else 'false'}
+        return self._post('{}/{}/members'.format(KLAVIYO_LIST_ENDPOINT, list_id), params)
+
+    def is_in_list(self, list_id, email):
+        params = {'api_key': self.api_token, 'email': email}
+        response = self._request('{}/{}/members'.format(KLAVIYO_LIST_ENDPOINT, list_id), urllib.parse.urlencode(params))
+        json_response = json.loads(response.text)
+        return json_response['page_size'] > 0
+
+    def remove_from_list(self, list_id, email):
+        params = {'api_key': self.api_token, 'email': email}
+        return self._post('{}/{}/members/exclude'.format(KLAVIYO_LIST_ENDPOINT, list_id), params)
+
     def _normalize_timestamp(self, timestamp):
         if isinstance(timestamp, datetime.datetime):
             timestamp = time.mktime(timestamp.timetuple())
@@ -92,6 +107,16 @@ class Klaviyo(object):
 
     def _request(self, path, params):
         server = self.api_server
-        response = requests.get('http://%s/%s?%s' % (server, path, params))
+        response = requests.get('https://%s/%s?%s' % (server, path, params))
+
+        if response.text == '1':
+            return True
+        if response.text == '0':
+            return Talse
+        return response
+
+    def _post(self, path, params):
+        print('https://%s/%s' % (self.api_server, path))
+        response = requests.post('https://%s/%s' % (self.api_server, path), data=params)
         
-        return response.text == '1'
+        return response
